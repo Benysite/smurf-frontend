2f2aec42aad5bc7f98692a7ea9f83e2b8c9cc470
Beny <benjamin2406.vtlbm@gmail.com>
2025-12-06 14:27:51 +0100

    Update analyze.js

--- scripts/analyze.js
+++ scripts/analyze.js
@@ -2,14 +2,8 @@ const BACKEND_URL = "https://smurf-backend-yg7w.onrender.com";
 
 async function analyzePlayer(gameName, tagLine, region = "eu") {
     const url = `${BACKEND_URL}/api/riot/analyze/${gameName}/${tagLine}?region=${region}`;
-
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-        return data;
-
-    } catch (err) {
-        console.error("Erreur analyse :", err);
-        return null;
-    }
+    const res = await fetch(url);
+    return await res.json();
 }
+
+window.analyzePlayer = analyzePlayer;
